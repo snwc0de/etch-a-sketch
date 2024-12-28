ffb4814be1ac4f037cae3e8ba30e2cca11a8c988
snwc0de <154846385+snwc0de@users.noreply.github.com>
2024-12-28 03:28:39 +0100

    UI Changes
    1. made each grid items display random colors on mouseover
    2. updated the games UI.

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function makeGrid(size) {
     for (let i = 1; i <= total; i++) {
         const grid = document.createElement("div");
         grid.classList.add(`item`);
-        grid.textContent = `${i}`;
+        // grid.textContent = `${i}`;
         gridContainer.appendChild(grid);
         grid.style.width = `${itemSize}%`;
         grid.style.height = `${itemSize}%`;
         
         document.querySelectorAll(".item").forEach((item) => {
             item.addEventListener("mouseover", (e) => {
-                e.target.style.cssText = "background-color: orange; color: black;"
+                e.target.style.backgroundColor = getRandomColor();
                 e.target.style.width = `${itemSize}%`;
                 e.target.style.height = `${itemSize}%`;
             });
@@ -24,12 +24,22 @@ function makeGrid(size) {
 };
 makeGrid(16);
 
+//generates random colors
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 
 //creates button for choosing grid number
 const btn = document.querySelector("#btn");
 const newBtn = document.createElement("button");
 newBtn.setAttribute("class", "newBtn");
-newBtn.textContent = "CREATE";
+newBtn.textContent = "NEW GAME";
 btn.appendChild(newBtn);
 
 newBtn.addEventListener("click", createGrid);
@@ -53,7 +63,9 @@ function createGrid () {
         alert("Number should not exceed 100!")
     } else if (size !== Number()) {
         alert("Input a Number!");
-    } 
+    } else if (size == null || size == undefined) {
+        alert("Input a number!");
+    }
 };
 
 //creates the reset button
@@ -69,7 +81,7 @@ resetBtn.addEventListener("click", resetGrid)
 function resetGrid() {
     document.querySelectorAll(".item").forEach(item => {
         item.style.color = "white";
-        item.style.backgroundColor = "green";
+        item.style.backgroundColor = "rgb(255, 255, 255)";
     })
 }
 
