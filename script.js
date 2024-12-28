cf38b596f95dfba5f8586b4cc31abdbaf34a3069
snwc0de <154846385+snwc0de@users.noreply.github.com>
2024-12-28 01:01:20 +0100

    Add the reset button and the whole code works as expected

--- script.js
+++ script.js
@@ -1,33 +1,76 @@
-const gridContainer = document.querySelector("#gridContainer");
 
+function makeGrid(size) {
+    const gridContainer = document.querySelector("#gridContainer");
+    let itemSize = (100 / size);
+    let total = size * size;
 
-for (let i = 1; i <= (16 * 16); i++) {
-    const grid = document.createElement("div");
-    grid.classList.add(`item${i}`);
-    gridContainer.appendChild(grid);
-    grid.textContent = `${i}`;
+    //creates grid
+    for (let i = 1; i <= total; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add(`item`);
+        grid.textContent = `${i}`;
+        gridContainer.appendChild(grid);
+        grid.style.width = `${itemSize}%`;
+        grid.style.height = `${itemSize}%`;
+        
+        document.querySelectorAll(".item").forEach((item) => {
+            item.addEventListener("mouseover", (e) => {
+                e.target.style.cssText = "background-color: orange; color: black;"
+                e.target.style.width = `${itemSize}%`;
+                e.target.style.height = `${itemSize}%`;
+            });
+        });
+    };  
 };
+makeGrid(16);
 
-const items = gridContainer.querySelectorAll("div")
-items.forEach((div) => {
-    div.addEventListener("mouseover", (e) => {
-        e.target.style.cssText = "background-color: orange; color: black"
-    })
-});
 
+//creates button for choosing grid number
 const btn = document.querySelector("#btn");
 const newBtn = document.createElement("button");
 newBtn.setAttribute("class", "newBtn");
-const resetBtn = document.createElement("button");
-resetBtn.classList.add("resetBtn");
+newBtn.textContent = "CREATE";
 btn.appendChild(newBtn);
 
-newBtn.addEventListener("click", removeGrid);
+newBtn.addEventListener("click", createGrid);
 
+//function to remove existing grid
 function removeGrid () {
+    //continuosly removes the first child of the container until none is left
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild)
     }
+};
+
+//function to create grid
+function createGrid () {
+    let size = prompt("WRITE A NUMBER", "Input number here")
+
+    if (size <= 100) {
+        removeGrid();
+        makeGrid(size);
+    } else if (size > 100) {
+        alert("Number should not exceed 100!")
+    } else if (size !== Number()) {
+        alert("Input a Number!");
+    } 
+};
+
+//creates the reset button
+const resetBtn = document.createElement("button");
+resetBtn.classList.add("resetBtn");
+resetBtn.textContent = "RESET";
+btn.appendChild(resetBtn);
+
+
+resetBtn.addEventListener("click", resetGrid)
+ 
+//function to reset grid
+function resetGrid() {
+    document.querySelectorAll(".item").forEach(item => {
+        item.style.color = "white";
+        item.style.backgroundColor = "green";
+    })
 }
 
 
